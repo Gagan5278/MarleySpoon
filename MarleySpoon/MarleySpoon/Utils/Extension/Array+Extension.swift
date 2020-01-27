90b7c00325744d47f2c502a5ccdf051fed3a74f3
Gagan Vishal Mishra <gagan5278@gmail.com>
2020-01-27 09:49:29 +0200

    minor description update

--- MarleySpoon/MarleySpoon/Utils/Extension/Array+Extension.swift
+++ MarleySpoon/MarleySpoon/Utils/Extension/Array+Extension.swift
@@ -9,6 +9,7 @@
 import Foundation
 extension Array where Element: Tags {
     
+    /// returns comma sprated tags 
     func createHumanRedableString() -> String {
         return self.reduce([String](), { res, item in
             var arr = res
